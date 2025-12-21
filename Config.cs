13a0e9c4dade653759b667f9bd2acf442beb6be1
SANADASHRAF <sanadbareen@gmai.com>
2025-12-21 15:53:11 +0200

    --up

--- Config.cs
+++ Config.cs
@@ -1,4 +1,135 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace csv
+//{
+//    public static class StudentsDistributionJson
+//    {
+//        public static string Value = @"
+//        {
+//            ""Stages"": [
+//              {
+//                ""StageName"": ""الأبتدائية"",
+//                ""Grades"": [
+//                  {
+//                    ""GradeName"": ""الأولى"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 27 },
+//                      { ""ClassName"": ""ا"", ""StudentCount"": 24 },
+//                      { ""ClassName"": ""د"", ""StudentCount"": 22 },
+//                      { ""ClassName"": ""و"", ""StudentCount"": 29 },
+//                      { ""ClassName"": ""ج"", ""StudentCount"": 23 },
+//                      { ""ClassName"": ""0"", ""StudentCount"": 25 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثانية"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""د"", ""StudentCount"": 26 },
+//                      { ""ClassName"": ""هـ"", ""StudentCount"": 21 },
+//                      { ""ClassName"": ""و"", ""StudentCount"": 28 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثالثة"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""ز"", ""StudentCount"": 30 },
+//                      { ""ClassName"": ""ح"", ""StudentCount"": 24 },
+//                      { ""ClassName"": ""ط"", ""StudentCount"": 22 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الرابعة"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 28 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 23 },
+//                      { ""ClassName"": ""ج"", ""StudentCount"": 27 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الخامسة"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 25 },
+//                      { ""ClassName"": ""د"", ""StudentCount"": 22 },
+//                      { ""ClassName"": ""هـ"", ""StudentCount"": 30 }
+//                    ]
+//                  },
+// {
+//                    ""GradeName"": ""السادسة"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 26 },
+//                      { ""ClassName"": ""د"", ""StudentCount"": 24 },
+//                      { ""ClassName"": ""هـ"", ""StudentCount"": 30 }
+//                    ]
+//                  }
+//                ]
+//              },
+
+//              {
+//                ""StageName"": ""الإعدادية"",
+//                ""Grades"": [
+//                  {
+//                    ""GradeName"": ""الأول الإعدادي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 28 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 22 },
+//                      { ""ClassName"": ""ج"", ""StudentCount"": 26 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثاني الإعدادي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 25 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 29 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثالث الإعدادي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 27 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 23 },
+//                      { ""ClassName"": ""ج"", ""StudentCount"": 21 }
+//                    ]
+//                  }
+//                ]
+//              },
+
+//              {
+//                ""StageName"": ""الثانوية"",
+//                ""Grades"": [
+//                  {
+//                    ""GradeName"": ""الأول الثانوي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 30 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 25 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثاني الثانوي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 22 },
+//                      { ""ClassName"": ""ج"", ""StudentCount"": 28 }
+//                    ]
+//                  },
+//                  {
+//                    ""GradeName"": ""الثالث الثانوي"",
+//                    ""Classes"": [
+//                      { ""ClassName"": ""أ"", ""StudentCount"": 29 },
+//                      { ""ClassName"": ""ب"", ""StudentCount"": 24 },
+//                      { ""ClassName"": ""د"", ""StudentCount"": 26 }
+//                    ]
+//                  }
+//                ]
+//              }
+//            ]
+//            }";
+//    }
+
+//}
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,186 +143,45 @@ namespace csv
         {
             ""Stages"": [
               {
-                ""StageName"": ""الأبتدائية"",
+                ""StageName"": ""ابتدائية الجامعة  صغار"",
                 ""Grades"": [
                   {
-                    ""GradeName"": ""الأولى"",
+                    ""GradeName"": ""الاول"",
                     ""Classes"": [
-                      { ""ClassName"": ""ب"", ""StudentCount"": 27 },
-                      { ""ClassName"": ""ا"", ""StudentCount"": 24 },
-                      { ""ClassName"": ""د"", ""StudentCount"": 22 },
-                      { ""ClassName"": ""و"", ""StudentCount"": 29 },
-                      { ""ClassName"": ""ج"", ""StudentCount"": 23 },
-                      { ""ClassName"": ""0"", ""StudentCount"": 25 }
+                      { ""ClassName"": ""ب"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""أ"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""د"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""ج"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""0"", ""StudentCount"": 1 }
                     ]
                   },
                   {
-                    ""GradeName"": ""الثانية"",
+                    ""GradeName"": ""الثاني"",
                     ""Classes"": [
-                      { ""ClassName"": ""د"", ""StudentCount"": 26 },
-                      { ""ClassName"": ""هـ"", ""StudentCount"": 21 },
-                      { ""ClassName"": ""و"", ""StudentCount"": 28 }
+                      { ""ClassName"": ""ب"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""أ"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""د"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""ج"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""ه"", ""StudentCount"": 1 }
                     ]
                   },
                   {
-                    ""GradeName"": ""الثالثة"",
+                    ""GradeName"": ""الثالث"",
                     ""Classes"": [
-                      { ""ClassName"": ""ز"", ""StudentCount"": 30 },
-                      { ""ClassName"": ""ح"", ""StudentCount"": 24 },
-                      { ""ClassName"": ""ط"", ""StudentCount"": 22 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الرابعة"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 28 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 23 },
-                      { ""ClassName"": ""ج"", ""StudentCount"": 27 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الخامسة"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 25 },
-                      { ""ClassName"": ""د"", ""StudentCount"": 22 },
-                      { ""ClassName"": ""هـ"", ""StudentCount"": 30 }
-                    ]
-                  },
- {
-                    ""GradeName"": ""السادسة"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 26 },
-                      { ""ClassName"": ""د"", ""StudentCount"": 24 },
-                      { ""ClassName"": ""هـ"", ""StudentCount"": 30 }
-                    ]
-                  }
-                ]
-              },
+                       { ""ClassName"": ""ب"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""أ"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""د"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""ج"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""ه"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""و"", ""StudentCount"": 1 },
+                      { ""ClassName"": ""0"", ""StudentCount"": 1 }
 
-              {
-                ""StageName"": ""الإعدادية"",
-                ""Grades"": [
-                  {
-                    ""GradeName"": ""الأول الإعدادي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 28 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 22 },
-                      { ""ClassName"": ""ج"", ""StudentCount"": 26 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الثاني الإعدادي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 25 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 29 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الثالث الإعدادي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 27 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 23 },
-                      { ""ClassName"": ""ج"", ""StudentCount"": 21 }
-                    ]
-                  }
-                ]
-              },
-
-              {
-                ""StageName"": ""الثانوية"",
-                ""Grades"": [
-                  {
-                    ""GradeName"": ""الأول الثانوي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 30 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 25 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الثاني الثانوي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 22 },
-                      { ""ClassName"": ""ج"", ""StudentCount"": 28 }
-                    ]
-                  },
-                  {
-                    ""GradeName"": ""الثالث الثانوي"",
-                    ""Classes"": [
-                      { ""ClassName"": ""أ"", ""StudentCount"": 29 },
-                      { ""ClassName"": ""ب"", ""StudentCount"": 24 },
-                      { ""ClassName"": ""د"", ""StudentCount"": 26 }
                     ]
                   }
                 ]
               }
             ]
             }";
-        //public static string Value = @"
-        //{
-        //  ""Stages"": [
-        //    {
-        //      ""StageName"": ""ابتدائية الجامعة صغار"",
-        //      ""Grades"": [
-        //        {
-        //          ""GradeName"": ""المرحلة الاولى"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""ب"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ا"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""د"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""و"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ج"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""0"", ""StudentCount"": 1 }
-        //          ]
-        //        },
-        //        {
-        //          ""GradeName"": ""المرحلة الثانية"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""د"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""هـ"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""و"", ""StudentCount"": 1 }
-        //          ]
-        //        },
-        //        {
-        //          ""GradeName"": ""المرحلة الثالثة"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""ز"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ح"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ط"", ""StudentCount"": 1 }
-        //          ]
-        //        }
-        //      ]
-        //    },
-        // {
-        //      ""StageName"": ""ابتدائية الجامعة صغار"",
-        //      ""Grades"": [
-        //        {
-        //          ""GradeName"": ""المرحلة الاولى"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""أ"", ""StudentCount"": 2 },
-        //            { ""ClassName"": ""ب"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ج"", ""StudentCount"": 1 }
-        //          ]
-        //        },
-        //        {
-        //          ""GradeName"": ""المرحلة الثانية"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""د"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""هـ"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""و"", ""StudentCount"": 1 }
-        //          ]
-        //        },
-        //        {
-        //          ""GradeName"": ""المرحلة الثالثة"",
-        //          ""Classes"": [
-        //            { ""ClassName"": ""ز"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ح"", ""StudentCount"": 1 },
-        //            { ""ClassName"": ""ط"", ""StudentCount"": 1 }
-        //          ]
-        //        }
-        //      ]
-        //    }
-        //  ]
-        //}";    
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using csv.Models;
 using csv.Services;
 using System.Security.Cryptography.X509Certificates;
 using System.Linq;
+using TeachersGenerator.Services;
+using TeachersGenerator.Models;
 
 class Program
 {
@@ -14,76 +16,95 @@ class Program
     {
         try
         {
-            //Console.WriteLine("Generating students based on JSON configuration...");
-            //Console.WriteLine();
+            Console.WriteLine("Select Generation Mode:");
+            Console.WriteLine("1. Students (Original)");
+            Console.WriteLine("2. Teachers (New)");
+            Console.Write("Enter choice (1 or 2): ");
+            var choice = Console.ReadLine();
 
-            //// Parse the JSON configuration
-            //var config = JsonSerializer.Deserialize<StudentsDistributionConfig>(
-            //    StudentsDistributionJson.Value,
-            //    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
-            //);
-
-            //if (config == null || config.Stages == null || config.Stages.Count == 0)
-            //{
-            //    Console.WriteLine("Error: Invalid JSON configuration.");
-            //    return;
-            //}
-
-            //// Display configuration summary
-            //Console.WriteLine("Configuration Summary:");
-            //Console.WriteLine("======================");
-            //foreach (var stage in config.Stages)
-            //{
-            //    Console.WriteLine($"\nStage: {stage.StageName}");
-            //    foreach (var grade in stage.Grades)
-            //    {
-            //        Console.WriteLine($"  Grade: {grade.GradeName}");
-            //        foreach (var classItem in grade.Classes)
-            //        {
-            //            Console.WriteLine($"    Class {classItem.ClassName}: {classItem.StudentCount} students");
-            //        }
-            //    }
-            //}
-            //Console.WriteLine();
-
-            //// Generate students based on configuration
-            //List<Student> students = DataGenerator.GenerateStudentsFromConfig(config);
-
-            //// Create output file with total student count
-            //string filePath = $"students_{students.Count}.csv";
-
-            //CsvExporter.ExportStudentsToCsv(students, filePath);
-
-            //Console.WriteLine($"\n✓ CSV file created at: {Path.GetFullPath(filePath)}");
-            //Console.WriteLine($"✓ Total students generated: {students.Count}");
-            //Console.WriteLine($"✓ Total families generated: {students.Select(s => s.Father.FatherId).Distinct().Count()}");
-            // Parse the JSON configuration
-            var config = JsonSerializer.Deserialize<StudentsDistributionConfig>(
-                StudentsDistributionJson.Value,
-                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
-            );
-
-            if (config == null || config.Stages == null || config.Stages.Count == 0)
+            if (choice == "2")
             {
-                Console.WriteLine("Error: Invalid JSON configuration.");
-                return;
+                Console.WriteLine("Generating Teachers...");
+
+                var teacherNamesAr = new List<string>
+                {
+                    "محمد سعيد",
+                    "مينا مدحت",
+                    "سند اشرف",
+                    "مجدي موسي",
+                    "طه احمد",
+                    "موسي محمد",
+                    "سامي محمد",
+                    "محسن محمد",
+                    "محمود محمد",
+                    "يوسف محمد",
+                    "زياد احمد",
+                    "شادي محمد",
+                    "خالد احمد",
+                    "فيصل محمد",
+                    "احمد زيدان",
+                    "ابو بكر رجب",
+                    "عمرو احمد",
+                    "مصطفي سامي",
+                    "ابراهيم احمد",
+                    "شريف احمد",
+                    "حازم احمد",
+                    "فهد محمد",
+                    "ايمن زيدان",
+                    "عمر محمد",
+                    "حسام حسن",
+                    "كريم ابراهيم",
+                    "اشرف احمد",
+                    "طارق مصطفي",
+                    "سعود احمد",
+                    "احمد طه",
+                    "مازن محمد",
+                    "ناصر ناصر",
+                    "سيد احمد",
+                    "محمد ابراهيم",
+                    "مدحت احمد",
+                    "سلمان الفرج"
+                };
+
+
+                var service = new TeacherGeneratorService();
+                var teachers = service.GenerateTeachers(teacherNamesAr);
+
+                string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string filePath = $"teachers_{teachers.Count}_{timestamp}.csv";
+                TeacherCsvExporter.ExportTeachersToCsv(teachers, filePath);
+
+                Console.WriteLine($"\n✓ Teachers CSV file created at: {Path.GetFullPath(filePath)}");
+                Console.WriteLine($"✓ Total teachers generated: {teachers.Count}");
             }
-
-            // Generate students based on configuration
-            List<Student> students = DataGenerator.GenerateStudentsFromConfig(config);
-
-            // Create output file with total student count
-            string filePath = $"students_{students.Count}.csv";
-
-            CsvExporter.ExportStudentsToCsv(students, filePath);
-
-            // Only print these 3 lines
-            Console.WriteLine($"\n✓ CSV file created at: {Path.GetFullPath(filePath)}");
-            Console.WriteLine($"✓ Total students generated: {students.Count}");
-            Console.WriteLine($"✓ Total families generated: {students.Select(s => s.Father.FatherId).Distinct().Count()}");
-
-
-
+            else
+            {
+                // Parse the JSON configuration
+                var config = JsonSerializer.Deserialize<StudentsDistributionConfig>(
+                    StudentsDistributionJson.Value,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                );
+    
+                if (config == null || config.Stages == null || config.Stages.Count == 0)
+                {
+                    Console.WriteLine("Error: Invalid JSON configuration.");
+                    return;
+                }
+    
+                // Generate students based on configuration
+                List<Student> students = DataGenerator.GenerateStudentsFromConfig(config);
+    
+                // Create output file with total student count
+                string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string filePath = $"students_{students.Count}_{timestamp}.csv";
+    
+                CsvExporter.ExportStudentsToCsv(students, filePath);
+    
+                // Only print these 3 lines
+                Console.WriteLine($"\n✓ CSV file created at: {Path.GetFullPath(filePath)}");
+                Console.WriteLine($"✓ Total students generated: {students.Count}");
+                Console.WriteLine($"✓ Total families generated: {students.Select(s => s.Father.FatherId).Distinct().Count()}");
+            }
         }
         catch (Exception ex)
         {
